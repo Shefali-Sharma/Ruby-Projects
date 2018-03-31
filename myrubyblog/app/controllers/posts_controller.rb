15c6936651d0b791100e9901dacbbfed793dde1c
Shefali-Sharma <shefali92@gmail.com>
2018-03-31 00:05:20 -0400

    Adding posts
    Adding posts

--- myrubyblog/app/controllers/posts_controller.rb
+++ myrubyblog/app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
     def index
+        @content_first = 'Hello! Welcome to my website!';
+        @content_second = 'This is some sample text for our awesome new Ruby Blog!';
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def show
+        
+    end
+    
+    def destroy
         
     end
 end
